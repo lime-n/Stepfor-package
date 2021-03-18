c28870ccad5f22de2f9b6dd0e0e951e31fe1a4e2
em-lime <68914515+lime-n@users.noreply.github.com>
2021-03-18 09:05:52 +0000

    Add files via upload

--- data-raw/weight.R
+++ data-raw/weight.R
@@ -1,1 +1,12 @@
+weight <- structure(list(wgt = c(64L, 71L, 53L, 67L, 55L, 58L, 77L, 57L,
+                                 56L, 51L, 76L, 68L), hgt = c(57L, 59L, 49L, 62L, 51L, 50L, 55L,
+                                                              48L, 42L, 42L, 61L, 57L), age = c(8L, 10L, 6L, 11L, 8L, 7L, 10L,
+                                                                                                9L, 10L, 6L, 12L, 9L), id = c("white", "white", "white", "white",
+                                                                                                                              "asian", "asian", "asian", "asian", "black", "black", "black",
+                                                                                                                              "black"), sex = structure(c(1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L,
+                                                                                                                                                          2L, 2L, 2L), .Label = c("female", "male"), class = "factor"),
+                         col = c("blue", "blue", "green", "green", "grey", "grey",
+                                 "mixed", "mixed", "brown", "brown", "hazel", "hazel")), row.names = c(NA,
+                                                                                                       -12L), class = "data.frame")
+
 
